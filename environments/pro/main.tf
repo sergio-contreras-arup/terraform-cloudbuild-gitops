1c6c27d7c23991484d6dedfa63023b5c32f96b46
Sergio Contreras <Sergio.Contreras@arup.com>
2025-10-01 19:27:42 +0200

    fix

--- environments/pro/main.tf
+++ environments/pro/main.tf
@@ -15,10 +15,3 @@ module "cloudsql_postgres" {
   user_name           = var.user_name
   deletion_protection = var.deletion_protection
 }
-
-# Denegar IPs externas a nivel de proyecto
-module "vm_external_ip_policy" {
-  source     = "../../modules/org_policy_vm_external_ip"
-  project_id = var.project_id
-}
-
