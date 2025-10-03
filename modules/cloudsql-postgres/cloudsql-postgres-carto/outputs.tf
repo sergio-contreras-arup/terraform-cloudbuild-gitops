fee40f51a18c7a4c13e501147134dd6514dce917
Oscar Sanz Sanchez <oscar.sanz@arup.com>
2025-10-03 13:46:27 +0200

    fix: outputs

--- modules/cloudsql-postgres/cloudsql-postgres-carto/outputs.tf
+++ modules/cloudsql-postgres/cloudsql-postgres-carto/outputs.tf
@@ -1,26 +1,26 @@
 output "instance_connection_name" {
   description = "Nombre de conexión (<PROJECT>:<REGION>:<INSTANCE>)"
-  value       = google_sql_database_instance.postgres.connection_name
+  value       = google_sql_database_instance.postgres_carto.instance_connection_name
 }
 
 output "public_ip_address" {
   description = "Dirección IP pública de la instancia (si fue habilitada)"
-  value       = try([for ip in google_sql_database_instance.postgres.ip_address : ip.ip_address if ip.type == "PRIMARY"][0], null)
+  value       = try([for ip in google_sql_database_instance.postgres_carto.ip_address : ip.ip_address if ip.type == "PRIMARY"][0], null)
 }
 
 output "database_name" {
   description = "Nombre de la base de datos creada"
-  value       = google_sql_database.app_db.name
+  value       = google_sql_database.app_db_carto.name
 }
 
 output "user_name" {
   description = "Usuario de aplicación"
-  value       = google_sql_user.app_user.name
+  value       = google_sql_user.app_user_carto.name
 }
 
 output "user_password" {
   description = "Contraseña generada para el usuario (sensible)"
-  value       = random_password.app_user_pwd.result
+  value       = random_password.app_user_pwd_carto.result
   sensitive   = true
 }
 
--- modules/networking/subnet-carto/outputs.tf
+++ modules/networking/subnet-carto/outputs.tf
@@ -1,9 +1,9 @@
 output "subnet_id" {
   description = "ID de la subred creada"
-  value       = google_compute_subnetwork.subnet.id
+  value       = google_compute_subnetwork.subnet_carto.id
 }
 
 output "subnet_link" {
   description = "Self link de la subred creada"
-  value       = google_compute_subnetwork.subnet.self_link
+  value       = google_compute_subnetwork.subnet_carto.self_link
 }
