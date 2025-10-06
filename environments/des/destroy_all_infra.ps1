b3b24880e10334f6c0c8bacfe5cbd7285c8bfd3e
Sergio Contreras <Sergio.Contreras@arup.com>
2025-10-06 22:09:45 +0200

    feat: script to destroy all infrastructure

---
+++ environments/des/destroy_all_infra.ps1
@@ -0,0 +1,11 @@
+﻿Write-Host "Preparando para eliminar toda la infraestructura en el entorno de desarrollo (des)..."
+terraform init
+Write-Host "🔧 Paso 1: Eliminando conexión de service networking y rango reservado..."
+terraform destroy -target=google_service_networking_connection.private_vpc_connection -target=google_compute_global_address.private_ip_alloc -auto-approve
+
+Write-Host "✅ Paso 1 completado."
+
+Write-Host "🧹 Paso 2: Eliminando el resto de la infraestructura..."
+terraform destroy -auto-approve
+
+Write-Host "Infraestructura eliminada por completo."
