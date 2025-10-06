4e450f6eb879c608b92a878a5edcb250aba52f1e
Oscar Sanz Sanchez <oscar.sanz@arup.com>
2025-10-06 14:44:47 +0200

    temporal: disable gke

--- environments/des/main.tf
+++ environments/des/main.tf
@@ -24,20 +24,20 @@ module "apis" {
 module "vpc_carto" {
   source = "../../modules/networking/vpc-carto"
 
-  project_id                      = var.project_id
-  vpc_name                        = "gke-vpc"
-  auto_create_subnetworks         = false
+  project_id                        = var.project_id
+  vpc_name                          = "gke-vpc"
+  auto_create_subnetworks           = false
   enable_private_service_connection = true # Required for CloudSQL private networking
 }
 
 module "subnet_carto" {
   source = "../../modules/networking/subnet-carto"
 
-  project_id     = var.project_id
-  subnet_name    = "gke-subnet-eu-west1"
-  ip_cidr_range  = "10.0.0.0/20"
-  region         = var.gke_location
-  network_id     = module.vpc_carto.vpc_id
+  project_id    = var.project_id
+  subnet_name   = "gke-subnet-eu-west1"
+  ip_cidr_range = "10.0.0.0/20"
+  region        = var.gke_location
+  network_id    = module.vpc_carto.vpc_id
 
   secondary_ip_ranges = [
     {
@@ -74,48 +74,48 @@ module "cloud_nat_carto" {
 module "cloudsql_postgres_carto" {
   source = "../../modules/cloudsql-postgres/cloudsql-postgres-carto"
 
-  project_id       = var.project_id
-  region           = var.region
-  instance_name    = "carto-des-environment"
-  database_version = "POSTGRES_15"
-  tier             = "db-custom-1-3840" # 1 vCPU, 3.75 GB RAM (mínimo 2GB)
-  disk_type        = "PD_SSD"
-  disk_size        = 20
+  project_id        = var.project_id
+  region            = var.region
+  instance_name     = "carto-des-environment"
+  database_version  = "POSTGRES_15"
+  tier              = "db-custom-1-3840" # 1 vCPU, 3.75 GB RAM (mínimo 2GB)
+  disk_type         = "PD_SSD"
+  disk_size         = 20
   availability_type = "ZONAL"
-  
+
   # Database and user (using defaults: carto/carto)
   database_name = "carto"
   user_name     = "carto"
-  
+
   # Security
   deletion_protection = false # Set to true for production
   require_ssl         = true
   ipv4_enabled        = false
   private_network     = module.vpc_carto.vpc_self_link
-  
+
   # Backups
-  backup_enabled                     = true
-  point_in_time_recovery_enabled     = true
-  transaction_log_retention_days     = 7
-  
+  backup_enabled                 = true
+  point_in_time_recovery_enabled = true
+  transaction_log_retention_days = 7
+
   # Monitoring
   query_insights_enabled = true
-  
+
   depends_on = [module.apis, module.vpc_carto]
 }
 
 # GKE Cluster
-module "gke" {
-  source = "../../modules/gke-cluster/gke-carto"
+# module "gke" {
+#   source = "../../modules/gke-cluster/gke-carto"
 
-  project_id       = var.project_id
-  location         = var.gke_location
-  cluster_name     = var.gke_cluster_name
-  release_channel  = var.gke_release_channel
-  network          = module.vpc_carto.vpc_self_link
-  subnetwork       = module.subnet_carto.subnet_self_link
-  resource_labels  = { env = "dev-carto" }
-}
+#   project_id       = var.project_id
+#   location         = var.gke_location
+#   cluster_name     = var.gke_cluster_name
+#   release_channel  = var.gke_release_channel
+#   network          = module.vpc_carto.vpc_self_link
+#   subnetwork       = module.subnet_carto.subnet_self_link
+#   resource_labels  = { env = "dev-carto" }
+# }
 
 module "storage_bucket_carto" {
   source = "../../modules/storage-bucket/storage-bucket-carto"
