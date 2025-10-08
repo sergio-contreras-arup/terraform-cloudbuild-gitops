47b2e613d15cca17350143982d75bb3a696dd388
Sergio Contreras <Sergio.Contreras@arup.com>
2025-10-08 15:41:56 +0200

    fix ranges for pods and services

--- modules/gke-cluster/gke-carto/main.tf
+++ modules/gke-cluster/gke-carto/main.tf
@@ -22,8 +22,8 @@ resource "google_container_cluster" "this" {
 
   # VPC-native (IP aliasing) ranges for Pods and Services
   ip_allocation_policy {
-    cluster_secondary_range_name  = var.pods_secondary_range_name
-    services_secondary_range_name = var.services_secondary_range_name
+    cluster_ipv4_cidr_block  = "10.56.0.0/14"  # Para Pods
+    services_ipv4_cidr_block = "10.60.0.0/20"  # Para Servicios
   }
 
   # Private cluster to avoid public IPs on nodes
