38d5515e9ccbe78a6044b65b1a72566a8d68db67
Sergio Contreras <Sergio.Contreras@arup.com>
2025-10-13 20:22:13 +0200

    fix: order delete resources

--- environments/des/delete_gcp_resources.sh
+++ environments/des/delete_gcp_resources.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 # Script to delete GCP resources in defined order
+gcloud -q storage buckets delete gs://pgoum-frontend gs://carto-storage-bucket/
+gcloud -q container clusters delete dev-carto-gke --zone europe-southwest1
+gcloud -q artifacts repositories delete backend-image-repository --location=europe-southwest1
+gcloud -q sql instances delete carto-des-environment
+gcloud -q compute forwarding-rules delete cloudsql-psc-endpoint --region=europe-southwest1
+gcloud -q compute addresses delete cloudsql-psc-endpoint-ip --region=europe-southwest1
 
-gcloud storage buckets delete gs://pgoum-frontend gs://carto-storage-bucket/
-gcloud container clusters delete dev-carto-gke --zone europe-southwest1
-gcloud artifacts repositories delete backend-image-repository --location=europe-southwest1
-gcloud compute routers nats delete gke-nat --region=europe-southwest1 --router="gke-nat-router"
-gcloud compute networks subnets delete gke-subnet-eu-west1 --region=europe-southwest1
-gcloud compute routers delete gke-nat-router --region=europe-southwest1
-gcloud compute addresses delete gke-vpc-private-ip-alloc # Seleccionar global
-gcloud compute networks delete gke-vpc
