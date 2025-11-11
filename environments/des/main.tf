8aa0b8fcca9af9cebb314a19bc790e6cdedd72aa
Oscar Sanz Sanchez <oscar.sanz@arup.com>
2025-11-11 11:23:03 +0100

    test

--- environments/des/main.tf
+++ environments/des/main.tf
@@ -182,7 +182,7 @@ module "cloud_run_service_pgoum" {
   repository            = var.artifact_repository_name_pgoum
   image                 = var.cloud_run_image_name_pgoum
   shared_network_name   = data.google_compute_network.shared.id
-  shared_subnet_name    = data.google_compute_subnetwork.shared.name
+  shared_subnet_name    = data.google_compute_subnetwork.shared.id
   service_account_email = var.service_account_email
 
   labels = {
--- modules/compute/cloud-run-pgoum/main.tf
+++ modules/compute/cloud-run-pgoum/main.tf
@@ -10,21 +10,12 @@ resource "google_cloud_run_v2_service" "default" {
       image = "${var.region}-docker.pkg.dev/${var.project_id}/${var.repository}/${var.image}:${var.tag}"
     }
 
-    service_account = var.service_account_email
-
     vpc_access {
-      connector = google_vpc_access_connector.connector.name
-      egress    = "ALL_TRAFFIC"
+      network_interfaces {
+        network    = var.shared_network_name
+        subnetwork = var.shared_subnet_name
+      }
     }
   }
 }
 
-resource "google_vpc_access_connector" "connector" {
-  name = "run-vpc"
-  subnet {
-    name = var.shared_subnet_name
-  }
-  min_instances = 2
-  max_instances = 3
-  region        = var.region
-}
