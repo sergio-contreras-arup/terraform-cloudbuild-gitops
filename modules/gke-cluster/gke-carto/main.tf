ec5831714edbd951e3a241c05ee3a56699d3492c
Oscar Sanz Sanchez <oscar.sanz@arup.com>
2025-11-12 10:31:02 +0100

    test

--- modules/gke-cluster/gke-carto/main.tf
+++ modules/gke-cluster/gke-carto/main.tf
@@ -34,7 +34,7 @@ resource "google_container_cluster" "this" {
 
   # Private cluster to avoid public IPs on nodes
   private_cluster_config {
-    # enable_private_nodes    = true
+    enable_private_nodes    = true
     enable_private_endpoint = true
     master_ipv4_cidr_block  = var.master_ipv4_cidr_block
   }
